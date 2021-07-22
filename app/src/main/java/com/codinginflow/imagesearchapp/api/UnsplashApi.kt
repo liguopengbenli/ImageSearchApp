25bbcbf5551b5c73b8fa39279ed44d7c65ef5d26
guopeng li <guopeng.li@technicolor.com>
2021-07-22 10:56:21 +0200

    finish recycle view display search result

--- app/src/main/java/com/codinginflow/imagesearchapp/api/UnsplashApi.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/api/UnsplashApi.kt
@@ -8,12 +8,13 @@ import retrofit2.http.Query
 interface UnsplashApi {
 
     companion object {
+
         const val BASE_URL = "https://api.unsplash.com/"
         const val CLIENT_ID = BuildConfig.unsplash_access_key
     }
 
 
-    @Headers("Accept-Version: v1", "Autorization: Client-ID $CLIENT_ID")
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
     @GET("search/photos")
     suspend fun searchPhotos(
         @Query("query") query: String,
--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPagingSource.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPagingSource.kt
@@ -1,5 +1,6 @@
 package com.codinginflow.imagesearchapp.data
 
+import android.util.Log
 import androidx.paging.PagingSource
 import com.codinginflow.imagesearchapp.api.UnsplashApi
 import retrofit2.HttpException
@@ -11,20 +12,26 @@ class UnsplashPagingSource(
     private val unsplashApi: UnsplashApi,
     private val query: String
 ) : PagingSource<Int, UnsplashPhoto>() {
+    companion object{
+        private const val TAG = "UnsplashPagingSource"
+    }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UnsplashPhoto> {
         val position = params.key ?: UNSPLASH_STRATING_PAGE_INDEX
         return try {
             val response = unsplashApi.searchPhotos(query, position, params.loadSize)
             val photos = response.results
+            Log.d(TAG, "lig load............")
             LoadResult.Page(
                 data = photos,
                 prevKey = if(position == UNSPLASH_STRATING_PAGE_INDEX) null else position-1,
                 nextKey = if(photos.isEmpty()) null else position+1
             )
         }catch (exception: IOException){
+            Log.d(TAG, "lig error IO............")
             LoadResult.Error(exception)
         }catch (exception: HttpException){
+            Log.d(TAG, "lig error HTTP............ $exception")
             LoadResult.Error(exception)
         }
 
